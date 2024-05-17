0b3a26a4dbcbeebe2bbe4c4424781c811c213407
Matheus Nascimento <ma_eduard@hotmail.com>
2024-05-16 23:34:19 -0300

    Adicionando projeto e dependencias, checkstyle e sonarCloud

---
+++ src/main/java/com/gerenciamento/solidariza/Main.java
@@ -0,0 +1,14 @@
+package com.gerenciamento.solidariza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EnableFeignClients
+//@EnableJpaAuditing
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+}
